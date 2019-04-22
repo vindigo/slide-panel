02fcf505ad397facec2f6ed4a350286d57e0f666
Richard Frausto <vindigo@gmail.com>
2019-04-22 15:18:32 -0700

    WIP working with proper delays

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,42 +7,48 @@ document.addEventListener("DOMContentLoaded", function () {
   // var trayControls = document.querySelector('[data-id="tray-controls"]');
 
   btnHome.addEventListener('click', function () {
-    closeTray(lastOpenTrayID);
-    openTray("tray-home");
+    var delayMS = closeTray(lastOpenTrayID) || 0;
+    openTray("tray-home", delayMS);
     lastOpenTrayID = "tray-home";
   });
 
   btnControls.addEventListener('click', function () {
-    closeTray(lastOpenTrayID);
-    openTray("tray-controls");
+    var delayMS = closeTray(lastOpenTrayID) || 0;
+    openTray("tray-controls", delayMS);
     lastOpenTrayID = "tray-controls";
   });
-
 });
 
-function openTray(trayID) {
+function openTray(trayID, delayMS) {
   if (!trayID) { return };
 
   var trayElem = document.querySelector('[data-id="' + trayID + '"]');
 
   if (!trayElem) { return };
 
-  trayElem.classList.remove('hidden');
-  trayElem.classList.add('slideInUp');
+  setTimeout(function () {
+    trayElem.classList.remove('hidden');
+    trayElem.classList.add('slideInUp');
+  }, delayMS);
 }
 
 function closeTray(trayID) {
+  var DELAY_MS = 1100;
   if (!trayID) { return };
 
   var trayElem = document.querySelector('[data-id="' + trayID + '"]');
 
   if (!trayElem) { return };
 
-  trayElem.classList.remove('slideInUp');
-  trayElem.classList.add('slideOutDown');
+  setTimeout(function () {
+    trayElem.classList.remove('slideInUp');
+    trayElem.classList.add('slideOutDown');
+  }, 0);
 
   setTimeout(function () {
     trayElem.classList.add('hidden');
     trayElem.classList.remove('slideOutDown');
-  }, 1100);
+  }, DELAY_MS);
+
+  return DELAY_MS;
 }
